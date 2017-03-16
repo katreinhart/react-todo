ad8649b8a3c22e7abdf208c76e4ea9b3b06faa09
Kat Reinhart <kat.reinhart@gmail.com>
2017-03-16 12:10:52 -0700

    Display static todos.

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,36 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'walk the dog'
+        },
+        {
+          id: 2,
+          text: 'clean the floor'
+        },
+        {
+          id: 3,
+          text: 'make lunch'
+        },
+        {
+          id: 4,
+          text: 'work on react'
+        }
+      ]
+    };
+  },
   render: function(){
+    var {todos} =  this.state;
+
     return (
-      <h1>Todo App</h1>
+      <div>
+        <TodoList todos={todos} />
+      </div>
     );
   }
 });

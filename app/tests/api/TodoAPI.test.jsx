3c7dc97ba10d7e456edeb45187666eae3921dcb4
Kat Reinhart <kat.reinhart@gmail.com>
2017-03-16 17:35:13 -0700

    Add filter by search text

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -54,7 +54,7 @@ describe('TodoAPI', () => {
   describe('filterTodos', () => {
     var todos = [{
       id: 1,
-      text: 'some text here',
+      text: 'text here',
       completed: true
     },
     {
@@ -74,6 +74,18 @@ describe('TodoAPI', () => {
     it('should not return all items if showCompleted is false', () => {
       var filteredTodos = TodoAPI.filterTodos(todos, false, '');
       expect(filteredTodos.length).toBe(1);
+    });
+    it('should sort non completed items to the top', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+    it('should return all todos if searchText empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
     });
   })
 });

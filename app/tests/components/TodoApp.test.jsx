0eac4e20791afec489bd93cd9c4d4f397b117164
Kat Reinhart <kat.reinhart@gmail.com>
2017-03-16 21:45:38 -0700

    Add created and completed timestamps.

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,19 +19,30 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(dummyText);
 
     expect(todoApp.state.todos[0].text).toBe(dummyText);
+
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: "Test features",
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     }
     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
     todoApp.setState({todos: [todoData]});
 
     expect(todoApp.state.todos[0].completed).toBe(false);
+
     todoApp.handleToggle(11);
+
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+
+    todoApp.handleToggle(11);
+
+    expect(todoApp.state.todos[0].completedAt).toBe(undefined);
   });
 });
